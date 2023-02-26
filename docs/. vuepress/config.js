0d5d9b71d5c62520a714ae68fd89d07efe412f14
Keanuuuuuu <93373675+Keanuuuuuu@users.noreply.github.com>
2023-02-26 15:04:30 +0800

    first

---
+++ docs/. vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  themeConfig: {
+      // logo: '/assets/img/hero.png',
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'Guide', link: '/about' },
+        { text: 'External', link: 'https://google.com' },
+      ]
+  }
+}
