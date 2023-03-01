640cfb2fb06cced8a3b7b54804ef39f17095841e
Keanuuuuuu <93373675+Keanuuuuuu@users.noreply.github.com>
2023-03-01 21:52:24 +0800

    1

---
+++ .temp/app-enhancers/global-components-3.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+
+Vue.component("Badge", () => import("D:\\Vue_project\\Vuepress\\node_modules\\@vuepress\\theme-default\\global-components\\Badge"))
+Vue.component("CodeBlock", () => import("D:\\Vue_project\\Vuepress\\node_modules\\@vuepress\\theme-default\\global-components\\CodeBlock"))
+Vue.component("CodeGroup", () => import("D:\\Vue_project\\Vuepress\\node_modules\\@vuepress\\theme-default\\global-components\\CodeGroup"))
+
+
+export default {}
---
+++ .temp/app-enhancers/global-components-4.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+
+Vue.component("CodeGroup", () => import("D:\\Vue_project\\Vuepress\\node_modules\\@vuepress\\theme-default\\global-components\\CodeGroup"))
+Vue.component("CodeBlock", () => import("D:\\Vue_project\\Vuepress\\node_modules\\@vuepress\\theme-default\\global-components\\CodeBlock"))
+Vue.component("Badge", () => import("D:\\Vue_project\\Vuepress\\node_modules\\@vuepress\\theme-default\\global-components\\Badge"))
+
+
+export default {}
